6a40871a52087cb5565b6471c1a7bc9cdffc2792
suparjito <suparjito@satnusa.com>
2021-03-17 15:13:42 +0700

    Added navigation bar animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card({ children }) {
   return (
-    <div className='mt-2 p-2 w-full transform hover:scale-105 transition ease-in-out duration-150 cursor-pointer'>
+    <div className='mt-2 p-2 w-full transform hover:scale-105 transition ease-in-out duration-300 cursor-pointer'>
       <div className='p-0 shadow-lg container rounded-md bg-white flex justify-between items-center flex-col h-full'>
         {children}
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,33 @@
 import React, { useState, useEffect} from 'react'
 import { NavLink } from "react-router-dom";
 
-function Dropdown({ toggle }) {
+function Dropdown({ isOpen, toggle }) {
+
+  const transformEffect = isOpen ? 'translate-y-16 z-10' : 'z-0 -translate-y-36'
   return (
     <div 
-      className="px-2 pt-2 pb-3 space-y-1 sm:px-3" 
+      className={`absolute w-full bg-gray-800 px-2 pt-2 pb-3 space-y-1 sm:px-3 transition-all ease-in-out duration-300 transform ${transformEffect} slide`} 
       onClick={toggle}
     >
       <NavLink 
         to='/' 
         exact 
         activeClassName='bg-gray-900 text-white'
-        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium transition duration-300 ease-in-out"
       >
         Now Playing
       </NavLink>
       <NavLink 
         to='/upcoming' 
         activeClassName='bg-gray-900 text-white'
-        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium transition duration-300 ease-in-out"
       >
         Upcoming
       </NavLink>  
       <NavLink 
         to='/theaters' 
         activeClassName='bg-gray-900 text-white'
-        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium transition duration-300 ease-in-out"
       >
         Theaters
       </NavLink>  
@@ -54,8 +56,8 @@ export default function Nav() {
   }
 
   return (
-    <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="flex justify-center bg-gray-800 h-16">
+      <div className="absolute w-full bg-gray-800 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 z-20">
         <div className="flex items-center justify-between h-16">
           <div>
             <h1 className="text-white">Tix ID Clone</h1>
@@ -100,7 +102,7 @@ export default function Nav() {
         </div>
       </div>
 
-      { isOpen && (
+      {(
         <Dropdown 
           isOpen={isOpen} 
           toggle={toggle}
