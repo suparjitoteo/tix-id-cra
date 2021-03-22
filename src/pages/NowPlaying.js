c66eee43f5b4dd7ada218bef2e25e07fd3c686fb
suparjito <suparjito@satnusa.com>
2021-03-22 10:59:50 +0700

    - Alignment issue on now playing

--- src/pages/NowPlaying.js
+++ src/pages/NowPlaying.js
@@ -13,11 +13,10 @@ export default function NowPlaying ({ city }) {
   }
 
   return (
-    <div className='flex flex-wrap items-start'>
+    <div className='flex flex-wrap'>
       { nowPlaying.map(movie => (
         <Link className='flex w-2/4 md:w-1/3 lg:w-1/4' key={movie.id} to={`/movie/${movie.id}`}>
           <Card>
-            {/*<div className='h-56 sm:h-h100 md:h-72 w-full'>*/}
             <img className='object-cover h-full rounded-t-lg w-full' src={movie.poster_path} alt={movie.title} />
             <p className='h-20 m-2 text-xs md:text-base'>{movie.title}</p>
             <div className='flex w-full justify-center'>
--- src/pages/Upcoming.js
+++ src/pages/Upcoming.js
@@ -16,12 +16,10 @@ export default function Upcoming ({city}) {
     <div className='flex flex-wrap'>
       {upcoming.map(movie => (
         <Link className='flex w-2/4 md:w-1/3 lg:w-1/4' key={movie.id} to={`/movie/${movie.id}`}>
-          <Card
-            key={movie.id}
-          >
-            <img className='rounded-md w-full' src={movie.poster_path} alt={movie.title}/>
+          <Card>
+            <img className='object-cover h-full rounded-t-lg w-full' src={movie.poster_path} alt={movie.title}/>
             <p className='h-20 m-2 text-xs md:text-base'>{movie.title}</p>
-            <div className='flex'>
+            <div className='flex w-full justify-center'>
               {movie.merchant.map(({ merchant_name }, index) => (
                 <MerchantTag key={index} merchant_name={merchant_name} />  
               ))}
